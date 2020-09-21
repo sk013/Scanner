4ef9e27fe6c893acd0cc6f52909ee76dccbfe167
sk013 <67577569+sk013@users.noreply.github.com>
2020-09-21 03:48:01 +0100

    Update BasicPortScanner.py

--- BasicPortScanner.py
+++ BasicPortScanner.py
@@ -5,12 +5,11 @@ from termcolor import colored
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = input("[*]Enter the host to scan")
-port = 80
 def portscanner(port):
 	if sock.connect_ex((host,port)):
-		print(colored("port %d is closed" % (port),'red'))
+		print(colored("[-]port %d is closed" % (port),'red'))
 	else:
-		print(colored("port %d is open" %(port),'green'))
+		print(colored("[+]port %d is open" %(port),'green'))
 portscanner(port)
-#for port in range(1,1000):
-#	portscanner(port)
+for port in range(1,1000):
+	portscanner(port)
